179e5079a79c1a88cb0359baeba8955666d5c38a
Mgg69 <145523671+Mgg69@users.noreply.github.com>
2023-11-15 04:55:49 +0000

    calc

---
+++ tonimashin.js
@@ -0,0 +1,6 @@
+function Multiply(){
+    let num1 = document.getElementById('first').value;
+    let num2 = document.getElementById('second').value;
+    let answer = num1 + num2;
+    document.getElementById('result').innerHTML = answer;
+}
